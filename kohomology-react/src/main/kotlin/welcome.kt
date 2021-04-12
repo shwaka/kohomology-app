2c01204ba9710fa7e201d7275590db51e300f7b6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-12 16:22:03 +0900

    modify react

--- kohomology-react/src/main/kotlin/welcome.kt
+++ kohomology-react/src/main/kotlin/welcome.kt
@@ -1,18 +1,23 @@
 import kotlinx.html.InputType
 import kotlinx.html.js.onChangeFunction
+import kotlinx.html.js.onClickFunction
 import org.w3c.dom.HTMLInputElement
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.dom.div
+import react.dom.form
 import react.dom.input
 
 external interface WelcomeProps : RProps {
     var name: String
 }
 
-data class WelcomeState(val name: String) : RState
+data class WelcomeState(
+    val name: String,
+    val display: String = "",
+) : RState
 
 @JsExport
 class Welcome(props: WelcomeProps) : RComponent<WelcomeProps, WelcomeState>(props) {
@@ -22,19 +27,33 @@ class Welcome(props: WelcomeProps) : RComponent<WelcomeProps, WelcomeState>(prop
     }
 
     override fun RBuilder.render() {
-        div {
-            +"Hello, ${state.name}"
-        }
-        input {
-            attrs {
-                type = InputType.text
-                value = state.name
-                onChangeFunction = { event ->
-                    setState(
-                        WelcomeState(name = (event.target as HTMLInputElement).value)
-                    )
+        form {
+            input {
+                attrs {
+                    type = InputType.text
+                    value = state.name
+                    onChangeFunction = { event ->
+                        setState(
+                            WelcomeState(name = (event.target as HTMLInputElement).value)
+                        )
+                    }
                 }
             }
+            input {
+                attrs {
+                    type = InputType.button
+                    value = "Submit"
+                    onClickFunction = { _ ->
+                        val display = "Submitted name is ${state.name}"
+                        setState(
+                            WelcomeState(name = state.name, display = display)
+                        )
+                    }
+                }
+            }
+        }
+        div {
+            +"${state.display}"
         }
     }
 }
