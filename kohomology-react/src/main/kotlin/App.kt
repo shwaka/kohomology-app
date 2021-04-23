b7108e4017c6c788d8d535acc758e7c1248f21a5
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2021-04-23 22:31:10 +0900

    improve display

--- kohomology-react/src/main/kotlin/App.kt
+++ kohomology-react/src/main/kotlin/App.kt
@@ -6,13 +6,13 @@ import react.dom.div
 
 external interface AppProps : RProps
 data class AppState(
-    val display: String = "",
+    val display: List<String> = listOf(),
 ) : RState
 
 @JsExport
 class App(props: AppProps) : RComponent<AppProps, AppState>(props) {
     init {
-        state = AppState("")
+        state = AppState()
     }
 
     override fun RBuilder.render() {
@@ -27,14 +27,18 @@ class App(props: AppProps) : RComponent<AppProps, AppState>(props) {
                     """.trimIndent()
                     printlnFun = { text ->
                         this@App.setState({ previousState ->
-                            val newText = previousState.display + text + "\n"
+                            val newText = previousState.display + listOf(text)
                             AppState(newText)
                         })
                     }
                 }
             }
             div {
-                +state.display
+                state.display.map {
+                    div {
+                        +it
+                    }
+                }
             }
         }
     }
